d35f8cec3e3fd68adee1a551e71e1122bffd2414
Linda <linda@nycdevshop.com>
2012-10-19 15:27:57 -0400

    working on subscription show page

--- config/initializers/stripe.rb
+++
@@ -1,4 +0,0 @@
-#set stripe API key below + make sure to add this file to the .gitignore
-
-#Stripe.api_key = " insert secret test key from Stripe Account"
-#STRIPE_PUBLIC_KEY = "add publishable test key"
