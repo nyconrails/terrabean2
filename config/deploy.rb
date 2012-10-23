431fcc54ad62677a3ad5bccb194aaff7de1ef3be
Alec Hartman <alec@alechartman.com>
2012-10-22 20:41:45 -0400

    deploy settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,21 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'bundler/capistrano'
+set :application, "terrabean"
+set :repository,  "git@github.com:nyconrails/terrabean2.git"
+set :user, "railsapps"
+set :use_sudo, false
+set :deploy_to, "/home/railsapps/public_html/#{application}"
 
-set :scm, :subversion
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+role :web, "terrabean.nycdevshop.com"                          # Your HTTP server, Apache/etc
+role :app, "terrabean.nycdevshop.com"                          # This may be the same as your `Web` server
+role :db,  "terrabean.nycdevshop.com", :primary => true # This is where Rails migrations will run
 
 # If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
 # namespace :deploy do
 #   task :start do ; end
 #   task :stop do ; end
@@ -23,3 +23,13 @@ role :db,  "your slave db-server here"
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
+load 'deploy/assets'
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
